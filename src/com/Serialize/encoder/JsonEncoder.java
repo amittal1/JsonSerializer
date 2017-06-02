ed512f65da3e932c59b6a705ed088da7292c801f
amittal1 <amittal1@stevens.edu>
2017-06-02 12:21:25 -0400

    Implemented string checking under Primitives, used getDeclaredField instead of getField to get private fields and tested for multiple classes

--- src/com/Serialize/encoder/JsonEncoder.java
+++ src/com/Serialize/encoder/JsonEncoder.java
@@ -5,6 +5,8 @@ package com.Serialize.encoder;
  * Author: Ankit Luv Mittal
  * Created: 05/31/2017
  * Description: Encode any object to json
+ * 				1. classRegistry is for specific classes which are registered with selected fields 
+ * 					into this project.
  * Dependency:Libraries
  * 				1. Gson Library: Provided by google for converting objects to json
  */
@@ -14,23 +16,62 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.lang.model.type.PrimitiveType;
 
 import com.Serialize.Interface.JsonSerializable;
 import com.google.gson.Gson;
+import com.ib.client.CommissionReport;
+import com.ib.client.Order;
+import com.ib.client.OrderState;
 import com.test.encoder.Logger;
 
 public class JsonEncoder {
 	static private Map <String, Field []> classRegistry;
 	static private Gson gson;
-	private static int test =0;
 	
 	static{
 		classRegistry = new HashMap<>();
 		gson =  new Gson();
+		
+		//Registering CommissionReport class
+		registerClass(CommissionReport.class, "m_execId", "m_commission", 
+				"m_currency","m_realizedPNL", "m_yield", "m_yieldRedemptionDate");
+
+		//Registering OrderState class
+		registerClass(OrderState.class, "m_status",  "m_initMargin", 
+				"m_maintMargin", "m_equityWithLoan", "m_commission", "m_minCommission", 
+				"m_maxCommission", "m_commissionCurrency", "m_warningText");
+		
+		//Registering Order class
+		registerClass(Order.class, "m_clientId", "m_orderId", "m_permId", 
+					"m_parentId", "m_action", "m_totalQuantity", "m_displaySize", 
+					"m_orderType", "m_lmtPrice", "m_auxPrice", "m_tif", "m_account");
+	}
+	
+	//Register class in class registry
+	//returns true if registered correctly 
+	//false if any field is not found or already present in classRegistry
+	@SuppressWarnings("rawtypes")
+	public static boolean registerClass(Class c,  String ...fieldNames ) {
+
+		Field [] fields = new Field [fieldNames.length];
+		
+		for (int i = 0; i < fieldNames.length; i++) {
+			try {
+				fields[i] = c.getDeclaredField(fieldNames[i]);
+			} catch (NoSuchFieldException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+		}
+		classRegistry.put(c.getName(), fields);
+		return true;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static Field [] getFields(Class c){
+		return c.getDeclaredFields();
 	}
 	
 	
@@ -50,22 +91,21 @@ public class JsonEncoder {
 		if(isPrimitiveOrWrapper(obj.getClass())){
 			result = String.valueOf(obj);
 		}
-		else if(obj instanceof String){
+		/*else if(obj instanceof String){
 			result = obj.toString();
-		}
+		}*/
 		else if(obj instanceof Collection || obj instanceof Map){
-			Logger.debug(Logger.getMethodTrace() + " "+  test ++ +" "+  obj);
+			 
 			Object temp_result;
 			if(obj instanceof Map){
-				Logger.debug(Logger.getMethodTrace() + " "+ test ++ +" "+   obj);
 					temp_result = new HashMap<>();
 				((Map) obj).forEach((k,v) -> {
 					//if key is not a string ignore it
-					((Map) temp_result).put(defaultProcessor(k), defaultProcessor(v));
+					if(isPrimitiveOrWrapper(k.getClass()))
+						((Map) temp_result).put(k, defaultProcessor(v));
 				});
 			}
 			else{
-				Logger.debug(Logger.getMethodTrace() + " "+ test ++ +" "+  obj);
 				temp_result = new ArrayList<>();
 				((Collection) obj).forEach((value)->{
 					((Collection) temp_result).add(defaultProcessor(value));
@@ -74,7 +114,6 @@ public class JsonEncoder {
 			result = temp_result.toString();
 		}
 		else{
-			Logger.debug(Logger.getMethodTrace() + " "+ test ++ +" "+ obj);
 			result = encodeObject(obj);
 		}
 		
@@ -82,8 +121,9 @@ public class JsonEncoder {
 	}
 	
 	
+	@SuppressWarnings("rawtypes")
 	private static boolean isPrimitiveOrWrapper(Class type) {
-		if((type.isPrimitive() && type != void.class) ||
+		if((type.isPrimitive() && type != void.class) || type == String.class ||
 		        type == Double.class || type == Float.class || type == Long.class ||
 		        type == Integer.class || type == Short.class || type == Character.class ||
 		        type == Byte.class || type == Boolean.class || type == String.class){
@@ -93,7 +133,7 @@ public class JsonEncoder {
 		return false;
 	}
 
-	public static String encodeObject(Object obj) {
+	private static String encodeObject(Object obj) {
 		Object result = null;
 
 		if(obj == null){
@@ -101,17 +141,13 @@ public class JsonEncoder {
 		}
 		
 		if(obj instanceof JsonSerializable){
-			Logger.debug(Logger.getMethodTrace() + " "+  test ++ +" checking interfaceImpl "+  obj);
 			result = ((JsonSerializable) obj).prepareJSON();
 		}
 		else if(implements_toString(obj)){
-			Logger.debug(Logger.getMethodTrace() + " "+  test ++ +" checking toString "+  obj);
 			result = obj.toString();
 		}
 		else{
-			Logger.debug(Logger.getMethodTrace() + " "+  test ++ +" all conditions false reflection use "+  obj);
 			result = JsonEncoder.serializedFieldsUsingReflection(obj);
-//			result = defaultProcessor(result);
 		}
 		
 		return defaultProcessor(result);
@@ -132,29 +168,33 @@ public class JsonEncoder {
 		return false;
 	}
 	
-	@SuppressWarnings("rawtypes")
 	private static Map<String, Object> serializedFieldsUsingReflection(Object obj) {
 		// TODO Auto-generated method stub
 		Field [] fields;
 		if(classRegistry.containsKey(obj.getClass().getName())){
+			Logger.log(Logger.getMethodTrace()+" From classRegistry");
 			fields= classRegistry.get(obj.getClass().getName());
 		}
 		else{
-			Class c = obj.getClass();
-			fields = c.getDeclaredFields();
+			fields = getFields(obj.getClass());
 		}
 		Map<String, Object> map = new HashMap<>();
 		
 		for(Field d: fields){
+			Logger.log(Logger.getMethodTrace()+" From for loop "+fields.length);
+			Logger.log("field: "+d.getName());
 			d.setAccessible(true);
 			try {
-				map.put(d.getName(), encodeObject(d.get(obj)));
+				Logger.log(Logger.getMethodTrace()+" From try");
+				map.put(d.getName(), defaultProcessor(d.get(obj)));
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				// TODO Auto-generated catch block
+				Logger.log(Logger.getMethodTrace()+" From catcch");
 				e.printStackTrace();
 				map.put("exception", e);
 			}
 		}
+		Logger.log(Logger.getMethodTrace()+" From end ");
 		return map;
 	}
 	
@@ -163,9 +203,4 @@ public class JsonEncoder {
 		return gson.toJson(obj);
 	}
 
-	
-	public static void main(String[] args) {
-		JsonEncoder jsonEncoder =  new JsonEncoder();
-		System.out.println(JsonEncoder.implements_toString(jsonEncoder));
-	}
 }
--- src/com/test/encoder/EncoderTest.java
+++ src/com/test/encoder/EncoderTest.java
@@ -1,5 +1,7 @@
 package com.test.encoder;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Author: Ankit Luv Mittal
  * Created: 05/31/2017
@@ -12,12 +14,18 @@ import java.util.List;
 import java.util.Map;
 
 import com.Serialize.encoder.JsonEncoder;
+import com.ib.client.CommissionReport;
+import com.ib.client.OrderState;
 
 public class EncoderTest {
 	Map<Integer, Object> map = new HashMap<>();
 	
+	public static void printFromJsonEncoder(Object obj){
+		System.out.println("here");
+		System.out.println(JsonEncoder.serializeToJson(obj));;
+	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		EncoderTest e = new EncoderTest();
 		//Map<Integer, Object> map = new HashMap<>();
 		for(int i=0; i<5; i++){
@@ -25,9 +33,54 @@ public class EncoderTest {
 			list.add("hello "+i);
 			e.map.put(i++, list);
 		}
-		//System.out.println(map.toString());
+		System.out.println(e.map.toString());
+		System.out.println(JsonEncoder.serializeToJson(e));
 		
-		System.out.println(JsonEncoder.serializeToJson(e.map));
+		
+		
+		
+		//Tested over these classes but are not part of the project, thus commented and just for reading
+		/*System.out.println(JsonEncoder.serializeToJson(e.map));
+		
+		//JsonEncoder.registerClass(IBMessage.class, "values", "messagekey_");
+
+		IBMessage m= new IBMessage("test");
+		m.add("hello", "world");
+		m.add("key", "value");
+		m.add("mittal", "ankit" );
+		System.out.println(JsonEncoder.serializeToJson(m));
+
+*/		
+		
+		
+		/*JsonEncoder.registerClass(OrderState.class, "m_status",  "m_initMargin", 
+						"m_maintMargin", "m_equityWithLoan", "m_commission", "m_minCommission", 
+						"m_maxCommission", "m_commissionCurrency", "m_warningText");
+		
+		OrderState cmm = OrderState.class.getConstructor(null).newInstance(null);
+		cmm.commission(100);
+		cmm.warningText("yoooooo");
+		cmm.initMargin("initMarginVal");
+		cmm.maintMargin("mainVal");;
+		cmm.maxCommission(100.00);
+		
+		System.out.println(JsonEncoder.serializeToJson(cmm));
+		*/
+		/*
+		 * 		JsonEncoder.registerClass(CommissionReport.class, "m_execId", "m_commission", 
+				"m_currency","m_realizedPNL", "m_yield", "m_yieldRedemptionDate");
+		
+		CommissionReport cmm = new CommissionReport();
+		cmm.m_commission = 100;
+		cmm.m_currency = "10";
+		cmm.m_execId= "1";
+		cmm.m_realizedPNL = 10.988;
+		cmm.m_yield = 1.1;
+		cmm.m_yieldRedemptionDate = 06012017;
+		
+		System.out.println(JsonEncoder.serializeToJson(cmm));
+
+		 */
 	}
 	
 }
